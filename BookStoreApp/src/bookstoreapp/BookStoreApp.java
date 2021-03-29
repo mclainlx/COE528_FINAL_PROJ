50e2275dcb550a16e15e2030e25b439ddfb46573
Alexander Mclain <alexmclain2001@gmail.com>
2021-03-29 15:23:28 -0400

    added login screen

--- BookStoreApp/src/bookstoreapp/BookStoreApp.java
+++ BookStoreApp/src/bookstoreapp/BookStoreApp.java
@@ -9,6 +9,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
@@ -19,11 +20,12 @@ public class BookStoreApp extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-        
+        Parent root = FXMLLoader.load(getClass().getResource("fxmlData/FXMLDocument.fxml"));
         Scene scene = new Scene(root);
-        
         stage.setScene(scene);
+        stage.setResizable(false); //makes main window not resizable
+        stage.setTitle("Book Store App");
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("images/book.png")));
         stage.show();
     }
 
--- BookStoreApp/src/bookstoreapp/FXMLDocumentController.java
+++ BookStoreApp/src/bookstoreapp/FXMLDocumentController.java
@@ -10,21 +10,49 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 /**
  *
  * @author danielkhan
  */
 public class FXMLDocumentController implements Initializable {
-    
+
+    @FXML Button logInButton;
+    @FXML TextField userTextField;
+    @FXML PasswordField passTextField;
+    @FXML Label badLoginLabel;
+
+
     @FXML
-    private Label label;
-    
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private void onLogin(ActionEvent event) {
+        System.out.println("Loggin in");
+        badLoginLabel.setVisible(false);
+
+        String user, pass;
+
+        user = userTextField.getText();//getting username and password
+        pass = passTextField.getText();
+
+        if(user.equals("admin") && pass.equals("admin")){
+            //set to admin scene
+            System.out.println("loggin in as admin");
+        }else{
+            if(customerLogin(user, pass)){
+                //set scene to customer scene
+                System.out.println("logging in as customer");
+            }else{
+                badLoginLabel.setVisible(true);
+            }
+        }
+
+    }
+
+    private boolean customerLogin(String user, String pass){
+        return false;
     }
     
     @Override
