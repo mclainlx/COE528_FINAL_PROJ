fa2e6e897de2c8d42de577f7d2d19bfd440e419b
Daniel Khan <daniel.khan_@hotmail.com>
2021-03-30 22:19:30 -0400

    added logout function and started customerScene

--- BookStoreApp/src/bookstoreapp/AdminSceneController.java
+++ BookStoreApp/src/bookstoreapp/AdminSceneController.java
@@ -1,15 +1,43 @@
 package bookstoreapp;
 
+import java.io.IOException;
 import javafx.fxml.Initializable;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
 
 public class AdminSceneController implements Initializable {
 
 
-
-
+    @FXML Button logoutButton1;
+    @FXML Button logoutButton2;
+    
+    @FXML
+    private void onLogout(ActionEvent event) {
+    
+    System.out.println("Loggin in");
+    
+            try {
+                Parent logInSceneRoot = FXMLLoader.load(getClass().getResource("fxmlData/logInScene.fxml"));
+                logoutButton1.getScene().setRoot(logInSceneRoot); //set the scene to the new scene or if failed to load, don't switch scene
+                logInSceneRoot.getScene().getWindow().sizeToScene();//resizes window to scene size
+                logoutButton2.getScene().setRoot(logInSceneRoot); //set the scene to the new scene or if failed to load, don't switch scene
+                logInSceneRoot.getScene().getWindow().sizeToScene();//resizes window to scene size
+            }catch (IOException e) {
+                System.out.println("failed to load LoginScene fxml file");
+                e.printStackTrace();
+            }
+    
+    
+    }
+  
+    
+    
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
