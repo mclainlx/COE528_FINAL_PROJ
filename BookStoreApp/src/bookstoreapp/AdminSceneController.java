156cfd6437fbae01c706570a511e8b8f29b53812
Alexander Mclain <alexmclain2001@gmail.com>
2021-04-01 19:57:03 -0400

    Added some AdminScene editBook integration

--- BookStoreApp/src/bookstoreapp/AdminSceneController.java
+++ BookStoreApp/src/bookstoreapp/AdminSceneController.java
@@ -10,17 +10,25 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 
 public class AdminSceneController implements Initializable {
 
 
+    @FXML public ListView customerListView;
+    @FXML public ListView bookListView;
     @FXML Button logoutButton1;
     @FXML Button logoutButton2;
-    
+
+    EditBook bookEditor;
+
+    @FXML
+    private void removeBook(ActionEvent event){
+
+    }
+
     @FXML
     private void onLogout(ActionEvent event) {
-    
-    System.out.println("Loggin in");
     
             try {
                 Parent logInSceneRoot = FXMLLoader.load(getClass().getResource("fxmlData/logInScene.fxml"));
@@ -35,11 +43,31 @@ public class AdminSceneController implements Initializable {
     
     
     }
-  
-    
+    @FXML
+    private void refresh(ActionEvent e){ //refreshes listviews within the admin screen
+        bookListView.getItems().clear();
+        for (Book b:bookEditor.getBooks()
+        ) {
+            bookListView.getItems().add(b);
+        }
+        customerListView.refresh();
+        bookListView.refresh();
+        System.out.println("Refreshing books and customers");
+    }
+
+    @FXML
+    private void addBook(ActionEvent e){ //adds a book using the EditBook object that we declared earlier
+        bookEditor.addBook();
+        System.out.println("Adding a book");
+    }
     
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        bookEditor = new EditBook();
+        bookEditor.loadBooks();
+        for (Book b:bookEditor.getBooks()
+             ) {
+            bookListView.getItems().add(b);
+        }
     }
 }
--- BookStoreApp/src/bookstoreapp/editCustomer.java
+++ BookStoreApp/src/bookstoreapp/editCustomer.java
@@ -11,6 +11,6 @@ import java.util.ArrayList;
  *
  * @author kasey
  */
-public class editCustomer {
+public class EditCustomer {
     
 }
