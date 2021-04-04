84322e0c2dffb8aeaccca6bbb12cbe659ebcff87
Daniel Khan <daniel.khan_@hotmail.com>
2021-04-04 01:13:32 -0400

    checkout scenelogout function

--- BookStoreApp/src/bookstoreapp/CheckoutSceneController.java
+++ BookStoreApp/src/bookstoreapp/CheckoutSceneController.java
@@ -6,12 +6,16 @@
 package bookstoreapp.fxmlData;
 
 import bookstoreapp.Customer;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.control.Label;
-
+import javafx.scene.control.Button;
 /**
  * FXML Controller class
  *
@@ -21,6 +25,7 @@ public class CheckoutSceneController implements Initializable {
     @FXML public Label totPrice;
     @FXML public Label points;
     @FXML public Label stat;
+    @ FXML Button logoutButton;
     /**
      * Initializes the controller class.
      */
@@ -28,6 +33,19 @@ public class CheckoutSceneController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }  
+    
+    @FXML
+    private void onLogout(ActionEvent event) {
+    
+        try {
+            Parent logInSceneRoot = FXMLLoader.load(getClass().getResource("fxmlData/logInScene.fxml"));
+            logoutButton.getScene().setRoot(logInSceneRoot);
+            logInSceneRoot.getScene().getWindow().sizeToScene();
+        }catch (IOException e) {
+            System.out.println("failed to load LoginScene fxml file");
+            e.printStackTrace();
+        }
+    }    
     
     public void initCart(double d){ //takes price data from cart
         String s = Double.toString(d);
